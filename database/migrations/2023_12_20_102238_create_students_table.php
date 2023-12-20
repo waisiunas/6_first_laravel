5831ca2323a476dada68f829545775e03f51b5ed
Awais Younis <waisi.unas@gmail.com>
2023-12-20 16:04:58 +0500

    migrations

--- database/migrations/2023_12_20_102238_create_students_table.php
+++ database/migrations/2023_12_20_102238_create_students_table.php
@@ -11,14 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('students', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string("address")->nullable();
-            $table->rememberToken();
+            $table->string("reg_no")->unique();
             $table->timestamps();
         });
     }
@@ -28,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('students');
     }
 };
